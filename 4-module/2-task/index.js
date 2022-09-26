fd1a7727e364ddcaf3d986f09d902ff083a39ce9
Rustam1992 <mutalipov.rustam@gmail.com>
2022-09-26 16:08:10 +0600

    3-4 module

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let i =0;
+ for(let tab of table.querySelectorAll('tr')){
+  tab.children[i].style.background="red";
+  i++
+ }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+    for (const iterator of table.rows) {
+        switch (iterator.cells[3].getAttribute("data-available")) {
+            case "true":
+                iterator.classList.add("available")
+                continue;
+            case 'false':
+                iterator.classList.add("unavailable")
+                continue;
+            case false:
+                iterator.hidden = true;
+            default:
+                iterator.setAttribute('hidden', 'hidden')
+        }
+        if (iterator.cells[1].innerHTML < 18) {
+            iterator.style = "text-decoration: line-through"
+        };
+
+        if (iterator.cells[2].innerHTML == "m") {
+            iterator.classList.add("male");
+        };
+        if (iterator.cells[2].innerHTML == "f") {
+            iterator.classList.add("female");
+        };
+        console.log(iterator.cells[2].class)
+    }
 }
+
+
